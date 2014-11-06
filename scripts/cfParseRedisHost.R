dd41e767cfe214ae5cc30414de02adf6835876e4
Tim Labeeuw and Weyman Fung <tlabeeuw@pivotal.io>
2014-11-05 16:10:25 -0800

    Clean up Cf-Run.sh
    - Converted cf-run.sh to a R script (cfRun.R)
    - Merged cfParseRedisHost and cfParseRedisPassword into the cfRun.

--- scripts/cfParseRedisHost.R
+++
@@ -1,5 +0,0 @@
-require ('rjson')
-
-vcap_services = fromJSON(Sys.getenv('VCAP_SERVICES'))
-
-cat(sprintf("%s:%s", vcap_services$`p-redis`[[1]]$credentials$host, vcap_services$`p-redis`[[1]]$credentials$port))
--- scripts/cfParseRedisPassword.R
+++
@@ -1,5 +0,0 @@
-require ('rjson')
-
-vcap_services = fromJSON(Sys.getenv('VCAP_SERVICES'))
-
-cat(vcap_services$`p-redis`[[1]]$credentials$password)
